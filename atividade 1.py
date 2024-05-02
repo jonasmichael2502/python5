bb60de54888e16a98e905bef5d8bca8a543ecf55
jonasmichael2502 <166136028+jonasmichael2502@users.noreply.github.com>
2024-05-02 13:31:22 +0000

    atividade 1

---
+++ atividade 1.py
@@ -0,0 +1,15 @@
+import os
+	
+os.system ("cls || clear")
+print("\solicitando dados ===")
+nome = input("digite seu nome: ")
+sexo = input("digite seu sexo (m ou f)")
+estadocivil = input("digite seu estado ")
+
+sexo = sexo.lower()
+estadocivil = estadocivil.lower()
+
+if sexo == "f" and estadocivil == "casada":
+	tempodecasamento =input
+
+
